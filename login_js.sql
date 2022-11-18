cfd673a9a459de450b61331527ba1d7eb3a20eda
Juan Daniel Valencia <nielvadev@gmail.com>
2022-11-18 11:38:54 -0500

    Finalizado

---
+++ login_js.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-11-2022 a las 21:59:50
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `login_js`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` varchar(20) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `direccion` varchar(50) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `fecha_nac` date NOT NULL,
+  `genero` varchar(1) DEFAULT NULL,
+  `usuario` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- server.js
+++ server.js
@@ -1,49 +1,84 @@
 const app = express()
 
-import mysql from 'mysql'
+app.use(cors())
+
+import cors from 'cors'
 import express from 'express'
-import bodyParser from 'body-parser'
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+import mysql from 'mysql2'
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 
 const port = 3000
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'login_js'
+});
+
+// LOGIN
 app.post('/login', function (req, res) {
   const username = req.body.username
   const password = req.body.password
-  console.log(username, password)
-  res.send(username + password)
+  
+  const sql = `SELECT * FROM usuarios WHERE usuario = '${username}' AND password = '${password}'`
+
+  connection.query(sql, function (error, results, fields) {
+    if (error) throw error;
+    if (results.length > 0) {
+
+      res.send({status: 'ok'})
+      console.log(username + ' Inicia sesión correctamente')
+        
+    } else {
+    
+      res.send({status: 'error'})
+      console.log('Usuario o contraseña incorrectos')
+    
+    }
+  });
 })
+
+//Registro
+app.post('/register', function (req, res) {
+  
+  const id = req.body.id
+  const nombre = req.body.nombre
+  const apellido = req.body.apellido
+  const direccion = req.body.direccion
+  const telefono = req.body.telefono
+  const nacimiento = req.body.nacimiento
+  const genero = req.body.genero
+  const usuario = req.body.usuario
+  const password = req.body.password
+
+  const sql = `SELECT * FROM usuarios WHERE usuario = '${usuario}' OR identificacion = '${id}'`
+  
+  connection.query(sql, function (error, results, fields) {
+    if(error) throw error;    
+    if (results.length > 0) {
+
+      res.send({status: 'error'})
+      console.log('Ya existe un usuario con ese nombre o identificación')
+      
+    } else {
+
+      const sql2 = `INSERT INTO usuarios (identificacion, nombre, apellido, direccion, telefono, nacimiento, genero, usuario, password) VALUES ('${id}','${nombre}','${apellido}','${direccion}','${telefono}','${nacimiento}','${genero}','${usuario}','${password}')`;
+      
+      connection.query(sql2, function (error, results, fields) {
+        if(error) throw error;
+        res.send({status: 'ok'})
+        console.log('Usuario registrado correctamente')
+        console.log(results)
+      });  
+    }
+  });
+});
+    
 
 app.listen(port, function () {
   console.log('Servidor activo desde puerto: ' + port)
+
 })
-
-
-
-
-
-
-
-
-
-
-
-// import { createConnection } from 'mysql';
-
-// var connection = createConnection({
-//     host     : 'localhost',
-//     database : 'login_js',
-//     user     : 'root',
-//     password : '',
-//     port: 3000
-// });
-
-// connection.connect();
- 
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
- 
-// connection.end();
